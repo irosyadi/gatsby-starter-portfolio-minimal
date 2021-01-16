302ccc6fbf272909f928ee53b6202bc8ccdbcbd1
Imron Rosyadi <mas.iyom@gmail.com>
2021-01-17 05:34:02 +0700

    Update index.js

--- config/index.js
+++ config/index.js
@@ -47,7 +47,7 @@ module.exports = {
   //mediumRssFeed:
   //  "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40konstantinmuenster",
   // rssFeed: "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fwww.theguardian.com%2Finternational%2Frss",
-  rssFeed: "https://irosyadi.netlify.app/rss.xml",
+  rssFeed: "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Firosyadi.netlify.app%2Frss.xml",
 
   shownArticles: 3,
 
